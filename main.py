42e92db8c933d16684420d4f5bdb59ba877c757f
Ruszkij <83695384+Ruszkij@users.noreply.github.com>
2022-01-05 14:33:41 +0100

    Optimized my code a bit
    Moved the scanning process to a separate function. Also fixed some bugs:
    -only scans if the message was from a setup channel

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import re
 
 prefix = "*"
 bot = commands.Bot(command_prefix=prefix)
-isSetup = False
+isFromCheckchannel = False
 vtclient = vt.Client(cfg.APIKEY)
 checkchannels =  []
 logchannel = 0
 
+#Called when the bot logs in
 @bot.event
 async def on_ready():
-    #Print in the console if the bot has connected to discord
     print(f'{bot.user} has connected to Discord!')
 
 #Sets the bot up in the channel(to send logs from the channel)
@@ -46,22 +46,12 @@ async def prefix(ctx, *arg):
         await ctx.send("Applying the new prefix: " + arg[0])
         bot.command_prefix=arg[0]
 
-#Check if the message contains a link scans it and if the logchannel was set up send a log and deletes the message
+#Called when someone sends a message
 @bot.event
 async def on_message(ctx):
     await bot.process_commands(ctx)
-    link = re.search("(?P<url>https?://[^\s]+)", ctx.content).group("url")
-    url_id = vt.url_id(link)
-    url = await vtclient.get_object_async("/urls/{}", url_id)
-    analysis = dict(url.last_analysis_stats)
-    l_channel = bot.get_channel(logchannel)
-    if analysis.get('malicious') or analysis.get('suspicious') > 0 and isSetup and len(checkchannels) != 0:
-        msg = discord.Embed(title="Scan Results",description="Scanned URL rating", color=0xC906E3)
-        msg.add_field(name="URL:",value=link,inline=False)
-        msg.add_field(name="Rating:",value="This URL is most likely malicious",inline=False)
-        msg.add_field(name="Author:",value=ctx.author,inline=False)
-        await l_channel.send(embed = msg)
-        await ctx.delete() 
+    #Scans the message if there is a URL then scans it with VirusTotal
+    await scan(ctx)
 
 #Sets up the logchannel
 @bot.command()
@@ -72,6 +62,34 @@ async def log(ctx):
     else:
         await ctx.send("This is my logchannel now:" + "<#" + str(ctx.channel.id) + ">")
         logchannel = ctx.channel.id
-        isSetup = True
+
+#Scans the url
+async def scan(ctx):
+    #Checks if the message contains a URL
+    try:
+        link = re.search("(?P<url>https?://[^\s]+)", ctx.content).group("url")
+        url_id = vt.url_id(link)
+        url = await vtclient.get_object_async("/urls/{}", url_id)
+        analysis = dict(url.last_analysis_stats)
+        l_channel = bot.get_channel(logchannel)
+        rate = analysis.get('malicious') + analysis.get('suspicious')
+        #Checks if the message was sent from a channel where the bot is set up
+        for x in checkchannels:
+            if x == ctx.channel.id:
+                isFromCheckchannel = True
+                #Sends a log if thr URL is suspicious there is a logchannel set up
+                #And the message was sent from a channel where the bot is set up
+                if rate > 0 and logchannel != 0 and isFromCheckchannel:
+                    msg = discord.Embed(title="Scan Results",description="Scanned URL rating", color=0xC906E3)
+                    msg.add_field(name="URL:",value=link,inline=False)
+                    msg.add_field(name="Rating:",value="This URL is most likely malicious",inline=False)
+                    msg.add_field(name="Author:",value=ctx.author,inline=False)
+                    await l_channel.send(embed = msg)
+                    await ctx.delete()
+            else:
+                print("Nothing to see cuz no setup there")
+    except AttributeError:
+        print("Not a Link")
+
 
 bot.run(cfg.TOKEN)
