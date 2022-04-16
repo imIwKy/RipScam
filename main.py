1e17aeeb89f8f942f952e1bec674523a64195f94
imIwKy <istu.thebest@gmail.com>
2022-04-16 14:33:05 +0200

    Cleaned the code a little bit and finished the status command so when someone mentions a user with a status the bot notifies them

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import re
 
 prefix = "*"
 bot = commands.Bot(command_prefix=prefix)
-isFromScanChannel = False
 vtClient = vt.Client(cfg.APIKEY)
-scanChannels =  []
+channelsWhereScanning =  []
 userStatuses = {}
-logChannel = 0
+loggingChannel = 0
 
 #Called when the bot logs in
 @bot.event
@@ -21,21 +20,21 @@ async def on_ready():
 #Sets the bot up in the channel(to send logs from the channel)
 @bot.command()
 async def setup(ctx):
-    if ctx.channel.id in scanChannels:
+    if ctx.channel.id in channelsWhereScanning:
         await ctx.send("I've been already set up in this channel")
     else:
         await ctx.send("Sucessfully set up in <#" + str(ctx.channel.id) + ">")
-        scanChannels.append(ctx.channel.id)
-    if logChannel == 0:
+        channelsWhereScanning.append(ctx.channel.id)
+    if loggingChannel == 0:
         await ctx.send("Make sure to setup a logchannel aswell using the 'log' - command")
 
 #Gets the list of the channels the bot has been set up
 @bot.command()
 async def channels(ctx):
         msg = discord.Embed(title="Channel list",description="All channels i've been set up", color=0xC906E3)
-        for x in scanChannels:
+        for x in channelsWhereScanning:
             msg.add_field(name="Channel:",value="<#" + str(x) + ">",inline=False)
-        msg.add_field(name="Logchannel:",value="<#" + str(logChannel) + ">", inline=False)
+        msg.add_field(name="Logchannel:",value="<#" + str(loggingChannel) + ">", inline=False)
         await ctx.send(embed = msg)
 
 #Changes the bots prefix
@@ -44,7 +43,7 @@ async def prefix(ctx, *arg):
     if not arg:
         await ctx.send("You must give a prefix")
     else:
-        await ctx.send("Applying the new prefix: " + arg[0])
+        await ctx.send("Prefix has been set to: " + arg[0])
         bot.command_prefix=arg[0]
 
 #Called when someone sends a message
@@ -53,44 +52,42 @@ async def on_message(ctx):
     await bot.process_commands(ctx)
     #Scans the message if there is a URL then scans it with VirusTotal
     await scan(ctx)
+    if "<" in ctx.content and ">" in ctx.content:
+        await checkforstatus(ctx)
 
-#Sets up the logChannel
+#Sets up the loggingChannel
 @bot.command()
 async def log(ctx):
-    global logChannel
-    if ctx.channel.id == logChannel:
+    global loggingChannel
+    if ctx.channel.id == loggingChannel:
         await ctx.send("Im already set up here")
     else:
-        await ctx.send("This is my logChannel now:" + "<#" + str(ctx.channel.id) + ">")
-        logChannel = ctx.channel.id
+        await ctx.send("The logchannel is now:" + "<#" + str(ctx.channel.id) + ">")
+        loggingChannel = ctx.channel.id
 
 #Scans the url
 async def scan(ctx):
-    #Checks if the message contains a URL
+#Checks if the message is a link
     try:
         link = re.search("(?P<url>https?://[^\s]+)", ctx.content).group("url")
+        isAUrl = True
+    except AttributeError:
+        isAUrl = False
+#Scans the url
+    if(isAUrl and ctx.channel.id in channelsWhereScanning):
         url_id = vt.url_id(link)
         url = await vtClient.get_object_async("/urls/{}", url_id)
         analysis = dict(url.last_analysis_stats)
-        l_channel = bot.get_channel(logChannel)
+        l_channel = bot.get_channel(loggingChannel)
         rate = analysis.get('malicious') + analysis.get('suspicious')
-        #Checks if the message was sent from a channel where the bot is set up
-        for x in scanChannels:
-            if x == ctx.channel.id:
-                isFromScanChannel = True
-                #Sends a log if thr URL is suspicious there is a logChannel set up
-                #And the message was sent from a channel where the bot is set up
-                if rate > 0 and logChannel != 0 and isFromScanChannel:
-                    msg = discord.Embed(title="Scan Results",description="Scanned URL rating", color=0xC906E3)
-                    msg.add_field(name="URL:",value=link,inline=False)
-                    msg.add_field(name="Rating:",value="This URL is most likely malicious",inline=False)
-                    msg.add_field(name="Author:",value=ctx.author,inline=False)
-                    await l_channel.send(embed = msg)
-                    await ctx.delete()
-            else:
-                print("[URLSCAN]Nothing to see cuz no setup there")
-    except AttributeError:
-        print("[URLSCAN]Not a Link")
+        #Sends a log if thr URL is suspicious there is a loggingChannel set up
+        if rate > 0 and loggingChannel != 0:
+            msg = discord.Embed(title="Scan Results",description="Scanned URL rating", color=0xC906E3)
+            msg.add_field(name="URL:",value=link,inline=False)
+            msg.add_field(name="Rating:",value="This URL is most likely malicious",inline=False)
+            msg.add_field(name="Author:",value=ctx.author,inline=False)
+            await l_channel.send(embed = msg)
+            await ctx.delete()
 
 #Sets a custom status for the user
 @bot.command()
@@ -98,11 +95,11 @@ async def status(ctx, *arg):
     if not arg:
         await ctx.send("Make sure to give a status too")
     else:
-        cUStat = ""
+        currentUserStatus = ""
         for x in arg:
-            cUStat += x + " "
-        await ctx.send("Setting your status to: " + cUStat)
-        userStatuses.update({str(ctx.author.id) : cUStat})
+            currentUserStatus += x + " "
+        await ctx.send("Set your status to: " + currentUserStatus)
+        userStatuses.update({str(ctx.author.id) : currentUserStatus})
     if userStatuses:
         print(userStatuses)
 
@@ -110,11 +107,19 @@ async def status(ctx, *arg):
 @bot.command()
 async def delstatus(ctx):
     if str(ctx.author.id) in userStatuses.keys():
-        await ctx.send("Deleting your status")
+        await ctx.send("Deleted your status sucessfully")
         del userStatuses[str(ctx.author.id)]
     else:
         await ctx.send("You dont have a status set")
     if userStatuses:
         print(userStatuses)
+
+async def checkforstatus(ctx):
+    mentionedId = str(ctx.content)
+    removableChars = "<>@"
+    for c in removableChars:
+        mentionedId=mentionedId.replace(c,"")
+    if mentionedId in userStatuses.keys() and str(ctx.author.id) != mentionedId:
+        await ctx.channel.send("This user is currently : " + userStatuses.get(mentionedId))
 
 bot.run(cfg.TOKEN)
